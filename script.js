31ef57de7e4d112e056e91a41d39c24fc42be33a
BlackReacon <dariaschmidt0510@gmail.com>
2025-04-14 07:41:42 +0200

    add api registration

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ let todoList = document.querySelector("#todo_list");
 let todoInput = document.querySelector("#in_todoName");
 let addButton = document.querySelector("#b_addTodo");
 
+// Get the modal
+let modal_registration = document.getElementById("registrat");
+let modal_login = document.getElementById("signin");
+
+// Get the button that opens the modal
+let btn_registration = document.getElementById("b_registration");
+let btn_login = document.getElementById("b_login");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+//Modal OK button
+let btn_r_ok = document.querySelector("#r_button");
+let btn_l_ok = document.querySelector("#l_button");
+
+//var register user data
+let r_username = document.querySelector("#r_username");
+let r_email = document.querySelector("#r_email");
+let r_userpassword = document.querySelector("#r_password");
+
+//var login user data
+let l_email = document.querySelector("#l_email");
+let l_userpassword = document.querySelector("#l_password");
+let bearerToken = "";
+
 let arrTask = [];
 
 //Event Listener
@@ -12,8 +37,12 @@ addButton.addEventListener("click", () => {
   addTodo(event);
 });
 
-//Function
+//Event Listener modal ok button
+btn_r_ok.addEventListener("click", () => {
+  register(event);
+}); 
 
+//Function
 function init() {
   if (localStorage.getItem("tasks")) {
     arrTask = JSON.parse(localStorage.getItem("tasks"));
@@ -66,17 +95,6 @@ function saveTask() {
 }
 
 //--------------------------------
-// Get the modal
-var modal_registration = document.getElementById("registrat");
-var modal_login = document.getElementById("signin");
-
-// Get the button that opens the modal
-var btn_registration = document.getElementById("b_registration");
-var btn_login = document.getElementById("b_login");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 btn_registration.onclick = function() {
     modal_registration.style.display = "block";
@@ -98,3 +116,69 @@ btn_login.onclick = function() {
     modal_login.style.display = "none";
    }
  }
+/* --------------------------------- */
+
+function register(event) {
+  event.preventDefault();
+//Api register post
+let api_register = {
+  name: r_username.value,
+  email: r_email.value,
+  password: r_userpassword.value
+}
+  fetch('http://192.168.178.43:8000/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(api_register)
+  })
+  .then(response => response.json())
+  .then(data => notification(data));
+}
+
+function notification(msg){
+  alert("Message from God:" + JSON.stringify(msg.nachricht));
+  modal_registration.style.display = "none";
+}
+
+//API login post
+let api_login = {
+  email: l_email.value,
+  password: l_userpassword.value
+}
+
+
+/* 11|7D1tnNjXCqxmtcGKEtAV0AwcwQWFvhbuXxvlo8nZccfa2b05   BEARER*/
+/* const register = {
+  name: 'kartoffel',
+  password: 'kartoffel',
+  email: 'kartoffel@kartoffel.obst'
+} 
+const login = {
+  email: 'kartoffel@kartoffel.obst',
+  password: 'kartoffel',
+}*/
+
+
+/*  fetch('http://192.168.178.43:8000/api/todos', {
+  method: 'GET',
+  headers:{
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Bearer 11|7D1tnNjXCqxmtcGKEtAV0AwcwQWFvhbuXxvlo8nZccfa2b05'
+  }
+ })
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  let token = blabla
+
+  function register
+  function login
+  function todo abrufen
+  function todo schreiben
+  function todo ebarbeiten
+  function prio 
+  function todo löschen */
